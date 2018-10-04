5d876f6e31fa0866f746c6b26fa7b2d4ac05b501
Asgedom1 <35047289+Asgedom1@users.noreply.github.com>
2018-10-04 11:33:28 -0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int a=0, b=1;
+		int c=a+b;
+		 System.out.println(a);
+		 System.out.println(b);
+		 
+		 
+		
+		for(int i=0; i<=10; i++) {
+			
+				c=a+b;
+				a=b;
+				b=c;
+				System.out.println(c);
+				
+				
+	}
+	}
+
+}
